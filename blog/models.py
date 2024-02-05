8cbb50a80b44588dee81a32d30646ff6494a1ab7
Kamila <kamieva_k@iuca.kg>
2024-02-05 16:50:18 +0600

    add post_detail changed

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 class Post (models.Model):
-    tiitle = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 255)
     slug = models.SlugField(max_length = 255)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
 def __str__(self):
-    return self.tiitle 
+    return self.title 
     
 
