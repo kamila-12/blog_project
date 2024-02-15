428d72815410d33b87ccba28f4dbb406287200da
Kamila <kamieva_k@iuca.kg>
2024-02-16 02:16:02 +0600

    Added Bootstarp templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,4 @@
 
-# from django.urls import path
-# from . import views
-# urlpatterns = [
-#     path('', views.post_list, name='post_list'),
-#     path('<int:pk>/', views.post_detail, name='post_detail'),
-#     path('', views.post_list, name='post_list'),
-#     path('post/new/', views.post_new, name='post_new'),
-# ]
 from django.urls import path
 from . import views
 
