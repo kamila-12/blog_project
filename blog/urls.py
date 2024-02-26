45d56078dbf0d89de3e53294221608558b4a94af
Kamila <kamieva_k@iuca.kg>
2024-02-26 17:19:57 +0600

    Class based views

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostDeleteView
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/',  PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    # path('', views.post_list, name='post_list'),
+    # path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # path('post/new/', views.post_new, name='post_new'),
+    # path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 # Добавление обработчика для медиа-файлов
 if settings.DEBUG:
--- blog/views.py
+++ blog/views.py
@@ -3,41 +3,86 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.utils import timezone
+from django.urls import reverse
+from django.urls import reverse_lazy
+
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+  
+class PostListView(ListView):
+    
+    model = Post
+    template_name = 'blog/post_list_all.html'
  
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+
+class PostDetailView(DetailView):
+    model = Post 
+    
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+    success_url = '/'
+
+    
+class PostEditView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.published_date = timezone.now()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.object.pk})
+    
+class PostDeleteView(DeleteView):
+    model = Post 
+    success_url = reverse_lazy('post_list')
+
+    
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
     
     
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
 
 
-def post_edit(request, pk):
-    # post = get_object_or_404(Post, pk=pk)
-    post = Post.objects.get(pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+# def post_edit(request, pk):
+#     # post = get_object_or_404(Post, pk=pk)
+#     post = Post.objects.get(pk=pk)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, request.FILES, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog/post_edit.html', {'form': form})
